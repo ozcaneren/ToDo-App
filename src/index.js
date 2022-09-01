fee87f1aa4a147ae63864796087e76943fbd2c5d
Eren <herenozcan17@gmail.com>
2022-09-02 01:17:15 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const DATA = [
+    { id: "todo-0", name: "Eat", completed: true },
+    { id: "todo-1", name: "Code", completed: true },
+    { id: "todo-2", name: "Sleep", completed: false },
+    { id: "todo-3", name: "Repeat", completed: false }
+];
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App tasks={DATA} />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
